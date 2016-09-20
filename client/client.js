10a34f759ef6aa28bf589e7acb2ac3b81eae2191
Rohit221990 <jangid.rohit70@gmail.com>
2016-09-20 22:31:38 +0530

    update code

--- client/client.js
+++ client/client.js
@@ -1,1 +1,26 @@
+import React, {PropTypes} from 'react'
+import { render } from 'react-dom'
+import App from '../js/components/App'
+import configureStore from '../js/store'
+import { Provider } from 'react-redux'
+ import * as Actions from '../js/actions';
+ import ReactDOM from 'react-dom'
+//var render = require('react-dom').require;
+
+
+let initialState = {
+	chat : [{
+		message:{text: ''}	
+	}]
+}
+
+let store = configureStore(initialState);
+
+
+ReactDOM.render(
+	<Provider store={store}>
+	<App/>
+	</Provider>,
+	document.getElementById('app')
+)
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,26 @@
+module.exports = {
+  entry: './client/client.js',
+  output: {
+    filename: 'bundle.js',
+    path: './dist',
+	publicPath:'/'
+  },
+  devtool: 'source-map',
+
+  module: {
+    loaders: [
+      {
+		test: /\.js$/,  //Rejex match between '/../'. it is match the file of postfix .js and perdorm below loader on it
+		loader : 'babel-loader', //{babel is compiler tool, compiler plugin. it is compiler jsx, ES6 in javascript}
+		exclude : /node_module/, //exculude node modules folder(it is the rejex)
+		//query tells the babel what to do for every  the .js file
+		query : {
+			presets : ['react', 'es2015','stage-2'],
+		    }	
+      }
+
+    ]
+  }
+
+};
 
